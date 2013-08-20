f680934ee2892675953f65c173fa5f836c0fd07e
duanhongyi <duanhongyi@sunmap.com>
2013-08-20 15:40:08 +0800

    修改上传文件bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from subprocess import check_call
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
+README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 
