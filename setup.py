9191a3759f90a0f51f1fcd0ec72c855a26f777da
duanhongyi <duanhyi@gmail.com>
2013-08-20 11:59:24 +0000

    change setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'nose',
+    'wapiti',
     'setuptools>=1.0',
 ]
 
@@ -19,15 +20,13 @@ setup(
 
     name='genius',
     description='genius中文分词 Chinese Segment On linear-chain CRF',
-    version='2.0.2',
+    version='2.0.3',
     author='duanhongyi',
     author_email='duanhyi@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     long_description=README + '\n\n' + CHANGES,
-    install_requires=[
-        'wapiti',
-    ],
+    install_requires=requires,
     dependency_links=[
         'git+https://github.com/duanhongyi/python-wapiti.git#egg=wapiti',
     ],
