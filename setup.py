53397b673e721f4c832d832ca59dbb7afe719a14
duanhongyi <duanhyi@gmail.com>
2013-08-23 10:25:04 +0000

    change setup version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     name='genius',
     description='genius中文分词 Chinese Segment On linear-chain CRF',
-    version='2.0.5',
+    version='2.0.6',
     author='duanhongyi',
     author_email='duanhyi@gmail.com',
     packages=find_packages(),
